aa55aed044cbec5ae3d1b6b554179dd74524accd
Caleb Willis <calebwillis50@yahoo.com>
2019-12-12 15:05:20 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,7 +3,7 @@ def square_array(array)
   results_array = []
   
   while counter < array.length do
-    results_array << array[counter]**
+    results_array << array[counter**]
     counter += 1
     
   end
